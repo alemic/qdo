08cc03169b906e29a8106a3a1384f3c02a3dcf75
Børge Nerdal Kjelsrud <borgenk@gmail.com>
2013-08-26 01:51:09 +0200

    Add queue length to admin interface.

--- web/web.go
+++ web/web.go
@@ -4,6 +4,8 @@ import (
 	"html/template"
 	"net/http"
 
+	"github.com/garyburd/redigo/redis"
+
 	"github.com/borgenk/qdo/db"
 	"github.com/borgenk/qdo/log"
 )
@@ -14,6 +16,7 @@ type Header struct {
 
 type Page struct {
 	Header Header
+	Length int
 }
 
 var templates = template.Must(template.ParseFiles("web/template/index.html"))
@@ -34,11 +37,14 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer c.Close()
 
+	len, err := Length()
+
 	h := Header{
 		Title: "QDo",
 	}
 	p := &Page{
 		Header: h,
+		Length: len,
 	}
 	renderTemplate(w, "index", p)
 }
@@ -46,4 +52,15 @@ func handler(w http.ResponseWriter, r *http.Request) {
 func Run(dbc db.Config) {
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8080", nil)
+}
+
+func Length() (int, error) {
+	c := db.Pool.Get()
+	err := c.Err()
+	if err != nil {
+		log.Error("", err)
+		return 0, err
+	}
+	defer c.Close()
+	return redis.Int(c.Do("LLEN", db.WaitingList))
 }
