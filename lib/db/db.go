4de7f70dd95ba8d88eb8a8d042eda4652d0c0c04
Børge Nerdal Kjelsrud <borgenk@gmail.com>
2013-09-25 23:28:54 +0200

    Set max number of Redis connections.

--- lib/db/db.go
+++ lib/db/db.go
@@ -9,16 +9,17 @@ import (
 )
 
 type Config struct {
-	Host string // Default database host.
-	Port int    // Default database port.
-	Pass string // Default database password.
-	Idx  int    // Default database index.
+	Host string
+	Port int
+	Pass string
+	Idx  int
 }
 
 var Pool *redis.Pool
 
 func ConnectPool(dbc Config) {
 	Pool = &redis.Pool{
+		MaxActive:   100,
 		IdleTimeout: 0,
 		Dial: func() (redis.Conn, error) {
 			c, err := redis.Dial("tcp", fmt.Sprintf("%s:%d", dbc.Host, dbc.Port))
