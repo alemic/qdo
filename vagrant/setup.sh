b1de39f5a47f96edc0928ffbf219f5dd95867036
Børge Nerdal Kjelsrud <borgenk@gmail.com>
2013-09-30 00:42:56 +0200

    Update Vagrant file for version 1.3.x

--- vagrant/setup.sh
+++ vagrant/setup.sh
@@ -1,45 +1,45 @@
 #!/usr/bin/env bash
 set -e
+
+# Exit if setup is already done (Vagrant 1.2.x backward compatibility).
+if [ -e "/opt/vagrant-installed" ]; then
+    exit 0
+fi
+mkdir /opt/vagrant-installed
+
+
 export DEBIAN_FRONTEND=noninteractive
 
 # Date
 # https://help.ubuntu.com/community/UbuntuTime
 TIMEZONE=$(head -n 1 "/etc/timezone")
-if [ $TIMEZONE != "Europe/Oslo" ]; then
-    echo "Europe/Oslo" | sudo tee /etc/timezone
-    sudo dpkg-reconfigure --frontend noninteractive tzdata
-fi
+echo "Europe/Oslo" | tee /etc/timezone
+dpkg-reconfigure --frontend noninteractive tzdata
 
-apt-get update -q
+
 apt-get install -q -y python-software-properties git
 
-# Docker.io
-if [ ! -e "/opt/docker" ]; then
-    mkdir /opt/docker
+# Docker
+sh -c "curl http://get.docker.io/gpg | apt-key add -"
+sh -c "echo deb https://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
+apt-get update -q
+apt-get install -q -y linux-image-extra-`uname -r` lxc-docker
 
-    sh -c "curl http://get.docker.io/gpg | apt-key add -"
-    sh -c "echo deb https://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
-    apt-get update -q
-    apt-get install -q -y linux-image-extra-`uname -r` lxc-docker
+echo "limit nofile 262144 262144" >> /etc/init/docker.conf
+sed -i.bak 's/docker -d/docker -d -r/' /etc/init/docker.conf
+service docker restart
+sleep 3
 
-    echo "limit nofile 262144 262144" >> /etc/init/docker.conf
-    service docker restart
-    sleep 2
-fi
 
 # Go development environment
-if [ ! -e "/opt/go-env" ]; then
-    mkdir /opt/go-env
+add-apt-repository -y ppa:duh/golang
+apt-get update -q -y
+apt-get install -q -y golang
 
-    add-apt-repository -y ppa:duh/golang
-    apt-get update -q -y
-    apt-get install -q -y golang
+# ~/go ~/go/src ~/go/pkg ~/go/bin
+mkdir /home/vagrant/go/pkg
+mkdir /home/vagrant/go/bin
+chown -R vagrant.vagrant /home/vagrant/go
 
-    # ~/go ~/go/src ~/go/pkg ~/go/bin
-    mkdir /home/vagrant/go/pkg
-    mkdir /home/vagrant/go/bin
-    chown -R vagrant.vagrant /home/vagrant/go
-
-    echo "" >> /home/vagrant/.bashrc
-    echo "export GOPATH=~/go" >> /home/vagrant/.bashrc
-fi
+echo "" >> /home/vagrant/.bashrc
+echo "export GOPATH=~/go" >> /home/vagrant/.bashrc
