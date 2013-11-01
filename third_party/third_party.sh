4dc653a27d97c109662a380e8131c868044ee175
Børge Nerdal Kjelsrud <borgenk@gmail.com>
2013-11-01 23:11:11 +0100

    Move import util script.

--- third_party/third_party.sh
+++ third_party/third_party.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-cd ../third_party
 find . -type f -print0 | xargs -0 sed -i 's/github.com\/syndtr\/goleveldb\/leveldb/github.com\/borgenk\/qdo\/third_party\/github.com\/syndtr\/goleveldb\/leveldb/g'
 find . -type f -print0 | xargs -0 sed -i 's/code.google.com\/p\/snappy-go\/snappy/github.com\/borgenk\/qdo\/third_party\/code.google.com\/p\/snappy-go\/snappy/g'
